051a4310846de1f6b2836bcf72d07ec72b2a3675
jolaya182 <jolaya182@gmail.com>
2020-01-26 04:33:21 -0500

    completed writing sql statements, commented code

--- src/index.js
+++ src/index.js
@@ -1,66 +1,69 @@
 /* eslint-disable consistent-return */
+/**
+ * title: index.js
+ *
+ * date: 1/26/2020
+ *
+ * author: Javier olaya
+ *
+ * description: this file solved problem 1 and 2 for the given exercise
+ */
+
 const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database('./sqLite/aHistory3.db');
 
-// db.serialize(()=>{
-
-// });
-
-// const viewDataSql = 'SELECT ObjectNumber,  Title FROM historicData';
-// db.all(viewDataSql, [], (err, row) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log(row);
-// });
-
 // PROB 1
 // delete numbers that are not of type number.number.number
 let sql = "DELETE FROM historicData WHERE ObjectNumber REGEXP '^[0-9.]*$' ";
-db.all(sql, [], (err, row) => {
+db.all(sql, [], err => {
   if (err) {
     return console.error(err.message);
   }
-  console.log(row);
 });
 
 console.log('created database');
 
 // PROB 2
-// create 2 new columns
+// create 2 new columns and add the new data to each columns using regex
 sql = 'ALTER TABLE historicData ADD COLUMN ObjectStartDate TEXT';
-db.all(sql, [], (err, row) => {
+db.all(sql, [], err => {
   if (err) {
     return console.error(err.message);
   }
-  console.log(row);
 });
 
 sql = 'ALTER TABLE historicData ADD COLUMN ObjectEndDate TEXT';
-db.all(sql, [], (err, row) => {
+db.all(sql, [], err => {
   if (err) {
     return console.error(err.message);
   }
-  console.log(row);
 });
 
 sql =
   "UPDATE historicData SET ObjectStartDate = CASE WHEN ObjectDate REGEXP '^[^-|CA]*$' THEN (ObjectDate REGEXP '^[^-|CA]*$') ELSE CASE WHEN  ObjectDate REGEXP '^[0-9]{4}-' THEN (ObjectDate REGEXP '^[0-9]{4}-') ELSE WHEN ObjectDate REGEXP '-s[0-9]{4}' THEN (ObjectDate REGEXP '^[0-9]{4}-') ELSE CASE WHEN ObjectDate REGEXP 'Ca.s[0-9]{4}' THEN (ObjectDate REGEXP '[0-9]{4}' -3) ELSE ObjectDate END WHERE ObjectDate";
-db.all(sql, [], (err, row) => {
+db.all(sql, [], err => {
+  if (err) {
+    return console.error(err.message);
+  }
+});
+
+sql =
+  "UPDATE historicData SET ObjectEndDate = CASE WHEN ObjectDate REGEXP '^[^-|CA]*$' THEN (ObjectDate REGEXP '^[^-|CA]*$') ELSE CASE WHEN  ObjectDate REGEXP '^[0-9]{4}-' THEN (ObjectDate REGEXP '-[0-9]{2}' + ObjectDate REGEXP '^[0-9]{4}') ELSE WHEN ObjectDate REGEXP '-s[0-9]{4}' THEN (ObjectDate REGEXP '[^d]d{4}s*$') ELSE CASE WHEN ObjectDate REGEXP 'Ca.s[0-9]{4}' THEN (ObjectDate REGEXP '[0-9]{4}') ELSE ObjectDate END WHERE ObjectDate";
+db.all(sql, [], err => {
+  if (err) {
+    return console.error(err.message);
+  }
+});
+
+// view all rows for the updated table
+const viewDataSql = 'SELECT ObjectNumber,  Title FROM historicData';
+db.all(viewDataSql, [], (err, row) => {
   if (err) {
     return console.error(err.message);
   }
   console.log(row);
 });
 
-sql =
-  "UPDATE historicData SET ObjectEndDate = CASE WHEN ObjectDate REGEXP '^[^-|CA]*$' THEN (ObjectDate REGEXP '^[^-|CA]*$') ELSE CASE WHEN  ObjectDate REGEXP '^[0-9]{4}-' THEN (ObjectDate REGEXP '-[0-9]{2}' + ObjectDate REGEXP '^[0-9]{4}') ELSE WHEN ObjectDate REGEXP '-s[0-9]{4}' THEN (ObjectDate REGEXP '[^d]d{4}s*$') ELSE CASE WHEN ObjectDate REGEXP 'Ca.s[0-9]{4}' THEN (ObjectDate REGEXP '[0-9]{4}') ELSE ObjectDate END WHERE ObjectDate";
-db.all(sql, [], (err, row) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log(row);
-});
 // close the database connection
 db.close();
